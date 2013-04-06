cecb9007ef32748d521bab749522a32035a8d9f0
Martin Stoppler <stoppe@stoppler.de>
2013-04-06 14:59:16 +0200

    rewrote list.php

--- list.php
+++ list.php
@@ -1,40 +1,84 @@
 <?php
-$KOM_SHOWSIDEMENU = true;
+$KOM_SHOWSIDEMENU = false;
 
 $database = new Database($dblink);
+$databaseGR = new Database($dblink);
+$databaseK = new Database($dblink);
 
 if (is_numeric($active['cat'])) {
     $database->setFilter("categories", $active['cat']);
-}
-if (is_numeric($active['party'])) {
-    $database->setFilter("parties", $active['party']);
+    $databaseK->setFilter("categories", $active['cat']);
+    $databaseGR->setFilter("categories", $active['cat']);
 }
 if (is_numeric($active['pstg'])) {
     $database->setFilter("pledgestatetypegroup", $active['pstg']);
+    $databaseK->setFilter("pledgestatetypegroup", $active['pstg']);
+    $databaseGR->setFilter("pledgestatetypegroup", $active['pstg']);
 }
 
+$databaseGR->setFilter("parties", array(1,2));
+$databaseK->setFilter("parties", array(3));
 
 $database->loadContent();
+$databaseGR->loadContent();
+$databaseK->loadContent();
 
-if (is_array($database->getIssues("name")) && count($database->getIssues("name")) > 0) {
+registerStyle('theme/css/list.css', true);
 
-    foreach ($database->getIssues("name") as $value) {
-        echo "<h3><a style=\"font-size:1.1em;\" href=\"".dolink("single", array("issueid" => $value->getID()))."\"><strong>".$value->getName()."</strong></a></h3><ul>";
+
+    $auswGR = new Analysis($databaseGR);
+    $auswK = new Analysis($databaseK);
+
+    /* Aktuelle Verteilung */
+    $nr = $auswGR->getCurrentNumberOfPledgestatetypes();
+    if (is_array($nr)) {
+        $compl = array_sum($nr);
+        foreach ($databaseGR->getPledgestatetypegroups() as $value0) {
+            foreach ($databaseGR->getPledgestatetypegroup($value0->getID())->getPledgestatetypeLinks() as $value) {
+                $group_nr[$value0->getID()] += $nr[$value->getID()];
+            }
+            $group_perc[$value0->getID()] = floor($group_nr[$value0->getID()]/$compl*100);
+        }
         
-        foreach ($database->getParties("order") as $value3) {
-            if (is_array($value->getPledgesOfParty($value3->getID()))) {
-                foreach ($value->getPledgesOfParty($value3->getID()) as $value2) {
-                    echo "<li><span style=\"color:".$value2->getParty()->getColour()."\">".$value2->getParty()->getName()."</span>: ".$value2->getName()."</li>";
+       $gesperc = array_sum($group_perc);
+       if ($gesperc < 100) {
+            foreach ($databaseGR->getPledgestatetypegroups() as $value0) {
+                if ($group_perc[$value0->getID()] > 0) {
+                    $group_perc[$value0->getID()] += (100-$gesperc);
+                    break;
+                }
+            }
+       }
+        
+    }
+    
+    
+    $Knr = $auswK->getCurrentNumberOfPledgestatetypes();
+    if (is_array($Knr)) {
+        $Kcompl = array_sum($Knr);
+        foreach ($databaseK->getPledgestatetypegroups() as $value0) {
+            foreach ($databaseK->getPledgestatetypegroup($value0->getID())->getPledgestatetypeLinks() as $value) {
+                $Kgroup_nr[$value0->getID()] += $Knr[$value->getID()];
+            }
+            $Kgroup_perc[$value0->getID()] = floor($Kgroup_nr[$value0->getID()]/$Kcompl*100);
+        }
+        
+        $Kgesperc = array_sum($Kgroup_perc);
+        if ($Kgesperc < 100) {
+            foreach ($databaseK->getPledgestatetypegroups() as $value0) {
+                if ($Kgroup_perc[$value0->getID()] > 0) {
+                    $Kgroup_perc[$value0->getID()] += (100-$Kgesperc);
+                    break;
                 }
             }
         }
 
-        echo "</ul>";
-        if ($value->getCurrentState()) {
-            echo "Status: ".$value->getCurrentState()->getName()."<br>";
-        }
     }
-} else {
-    echo "Keine Einträge vorhanden.";
-}
+
+
+   
+
+
+
+include('templates/list.php');
 ?>
---
+++ templates/list.php
@@ -0,0 +1,57 @@
+<div class="list">
+<div class="listside">
+<?php if (is_array($nr)) { ?>
+<div>
+<h2>Wahlversprechen:</h1>
+    <div class="meter">
+        <? if ($group_perc[2] > 0) { ?><div style="background-color:green;width:<? echo $group_perc[2]; ?>%">&nbsp;</div><? } ?>
+        <? if ($group_perc[1] > 0) { ?><div style="background-color:#fbfbfb;width:<? echo $group_perc[1]; ?>%">&nbsp;</div><? } ?>
+        <? if ($group_perc[3] > 0) { ?><div style="background-color:red;width:<? echo $group_perc[3]; ?>%">&nbsp;</div><? } ?>
+    </div>
+    <ul>
+        <li><?=$group_nr[2];?> Versprechen wurden umgesetzt (<?=$group_perc[2];?>%)</li>
+        <li><?=$group_nr[3];?> Versprechen wurden gebrochen (<?=$group_perc[3];?>%)</li>
+        <li>Bei <?=$group_nr[1];?> Versprechen ist noch nichts passiert (<?=$group_perc[1];?>%)</li>
+    </ul>
+</div>
+<? } ?>
+<?php if (is_array($Knr)) { ?>
+<div>
+<h2>Koalitionsvertrag:</h1>
+    <div class="meter">
+        <? if ($Kgroup_perc[2] > 0) { ?><div style="background-color:green;width:<? echo $Kgroup_perc[2]; ?>%">&nbsp;</div><? } ?>
+        <? if ($Kgroup_perc[1] > 0) { ?><div style="background-color:#fbfbfb;width:<? echo $Kgroup_perc[1]; ?>%">&nbsp;</div><? } ?>
+        <? if ($Kgroup_perc[3] > 0) { ?><div style="background-color:red;width:<? echo $Kgroup_perc[3]; ?>%">&nbsp;</div><? } ?>
+    </div>
+    <p>Von <strong><?=$Kcompl;?> Punkten</strong> im Koalitionsvertrag wurden bisher <strong><?=$Kgroup_nr[2];?></strong> umgesetzt. </p>
+</div>
+<? } ?>
+</div>
+
+
+
+<?php
+if (is_array($database->getIssues("name")) && count($database->getIssues("name")) > 0) {
+
+    foreach ($database->getIssues("name") as $value) {
+        echo "<h3><a style=\"font-size:1.1em;\" href=\"".dolink("single", array("issueid" => $value->getID()))."\"><strong>".$value->getName()."</strong></a></h3><ul>";
+        
+        foreach ($database->getParties("order") as $value3) {
+            if (is_array($value->getPledgesOfParty($value3->getID()))) {
+                foreach ($value->getPledgesOfParty($value3->getID()) as $value2) {
+                    echo "<li><span style=\"color:".$value2->getParty()->getColour()."\">".$value2->getParty()->getName()."</span>: ".$value2->getName()."</li>";
+                }
+            }
+        }
+
+        echo "</ul>";
+        if ($value->getCurrentState()) {
+            echo "Status: ".$value->getCurrentState()->getName()."<br>";
+        }
+    }
+} else {
+    echo "Keine Einträge vorhanden.";
+}
+?>
+
+</div>
